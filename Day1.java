079e04e2777647ec100c37c1dc669eb3dc0c32c0
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-08 08:49:40 -0700

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,6 +1,6 @@
 package Steven;// Main Class bernama Steven
 public class Steven { // Main Class bernama Steven
     public static void main(String[] args) { // Method main 
-        System.out.println("Hello World"); //  Mencetak teks "World Hello"
+        System.out.println("Hello World"); //  Mencetak teks "Hello World"
     }
    }
