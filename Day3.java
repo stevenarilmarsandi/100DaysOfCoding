e74a618c5b34cb034428add05bba37bb5bdaabdd
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-09 09:20:27 -0700

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+package Steven;
+public class Steven {
+    public static void main(String[] args) {
+//Komentar Satu Baris
+
+/*
+Komentar Multibaris 
+Atau
+Lebih dari satu baris
+*/
+    }
+    
+}
