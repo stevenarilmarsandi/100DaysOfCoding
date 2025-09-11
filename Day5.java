4b0fac2c7f67c86d7a3965645c38ef5e72d57589
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-11 16:51:30 -0700

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,16 @@
+package bilanganbulat;
+public class BilanganBulat {
+ public static void main(String[] args) {
+        byte Bilanganbulat1 = 1;
+        short Bilanganbulat2 = 2;
+        int Bilanganbulat3 = 3;
+        long Bilanganbulat4 = 4;
+        
+        System.out.println(Bilanganbulat1);
+        System.out.println(Bilanganbulat2);
+        System.out.println(Bilanganbulat3);
+        System.out.println(Bilanganbulat4);
+        
+    }
+    
+}
