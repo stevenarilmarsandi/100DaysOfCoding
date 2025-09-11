a6fd18cb58b901b08aa3eef7217b028cf01f7a26
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-10 17:58:40 -0700

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,16 @@
+
+package Steven;
+
+public class main {
+
+        public static void main(String[] args) {
+        System.out.println("hello\nworld");
+        System.out.println("hello\tdunia");
+        System.out.println("\'Dunia\'");
+        System.out.println("\"Aku\"");
+        System.out.println("print\bworld");
+        System.out.println("print\\world");
+        
+    }
+    
+}
