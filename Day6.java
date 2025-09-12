ffbd36a095cd3840f853795d5db0726b29d767fc
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-12 09:13:18 -0700

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+package tipedatapecahan;
+public class TipeDataPecahan {
+public static void main(String[] args) {
+        float pecahan1 = 2.5f;
+        float pecahan2 = (float) 5.2;
+        double pecahan3 =  1234.5678;
+        
+       System.out.println(pecahan1); 
+       System.out.println(pecahan2);
+       System.out.println(pecahan3);
+    }
+    
+}
