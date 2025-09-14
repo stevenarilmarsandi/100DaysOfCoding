d7859cd385d146222606e5fcea8a34c99d88113b
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-13 23:03:42 -0700

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,18 @@
+package tipedatakarakter;
+public class TipeDataKarakter {
+public static void main(String[] args) {
+        char gender = 'L';
+        char bilangan = '1';
+        char operasiarimatika = '+';
+        
+        boolean benar = true;
+       boolean salah = false;
+       
+       System.out.println(gender);
+       System.out.println(bilangan);
+       System.out.println(operasiarimatika );
+       System.out.println(benar);
+       System.out.println(salah);
+    }
+    
+}
