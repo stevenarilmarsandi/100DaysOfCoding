70d0e40a17e8e2002fa353c5f62b6e8a7c981a08
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-14 09:27:39 -0700

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,10 @@
+package PengenalanString;
+public class PengenalanString {
+    public static void main(String[] args) {
+        String nama = "Steven Ariel Marsandi";
+        String hobi = "Bermain game dan bermain basket";
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Hobi saya : " + hobi);
+    }
+}
