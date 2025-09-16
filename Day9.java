de2d09916f8c8aa8a143a2d655c36ac0e2b2bbd3
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-15 18:36:41 -0700

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+package hehe;
+public class hehe{
+public static void main(String[] args) {
+      long harga = 10000;  
+        System.out.println("Harga awal: " + harga);
+        harga = 12000;
+        System.out.println("Harga setelah naik: " + harga);
+    }
+}
+
+  
