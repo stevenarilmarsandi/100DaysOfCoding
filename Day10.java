eef32f3b9576e53d4f2715aae8a105d999154747
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-17 05:28:02 -0700

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+package constantafinal;
+public class ConstantaFinal {
+public static void main(String[] args) {
+        final int maxumur= 100;
+        int umurSekarang = 20;
+
+        System.out.println("Umur sekarang: " + umurSekarang);
+        System.out.println("Umur maksimal: " + maxumur);
+    }
+}
+
