af1b65e74ea17864bfd2c0571cbff363ca5d0777
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-22 23:17:36 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,24 @@
+package Steven;
+
+public class Steven {
+    public static void main(String[] args) {
+      
+        int angka1 = 10;
+        int angka2 = 5;
+        int angka3 = 4;
+        int angka4 = 3;
+
+
+        int hasilTambah = angka1 + angka2;
+
+
+        int hasilKurang = angka3 - angka4;
+
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua  : " + angka2);
+        System.out.println("Angka ketiga  : " + angka3);
+        System.out.println("Angka keempat  : " + angka4);
+        System.out.println("Hasil Penjumlahan (10 + 5) = " + hasilTambah);
+        System.out.println("Hasil Pengurangan (4 - 3) = " + hasilKurang);
+    }
+}
