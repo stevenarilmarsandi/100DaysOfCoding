c8e0ef6fcf980cf38e93c940a14637f583fcfb5e
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-23 06:49:10 -0700

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,13 @@
+public class Aritmatika {
+    public static void main(String[] args) {
+        int angka1 = 2;
+        int angka2 = 5;
+
+        int hasilKali = angka1 * angka2;
+        int hasilBagi = angka1 / angka2;
+        System.out.println("Angka 1 = " + angka1);
+        System.out.println("Angka 2 = " + angka2);
+        System.out.println("Hasil Perkalian = " + hasilKali);
+        System.out.println("Hasil Pembagian = " + hasilBagi);
+    }
+}
