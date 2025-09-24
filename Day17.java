8794dca3433816d13ceb1fba576fed9a87fba81d
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-24 08:14:05 -0700

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,12 @@
+public class Modulus {
+    public static void main(String[] args) {
+        int Angka a = 4;
+        int Angka b = 3;
+
+        int hasil = a % b;
+
+        System.out.println("Angka a = " + Angka a);
+        System.out.println("Angka b = " + Angka b);
+        System.out.println(" hasil (a % b) = " + hasil);
+    }
+}
