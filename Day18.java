75eee1a10728013ca70deb82df804c886c40b474
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-24 09:06:58 -0700

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,17 @@
+public class KonversiOtomatis {
+    public static void main(String[] args) {
+        byte a = 22;         
+        short b = a;         
+        int c = b;           
+        long d = c;          
+        float e = d;         
+        double f = e;        
+
+        System.out.println("byte   : " + a);
+        System.out.println("short  : " + b);
+        System.out.println("int    : " + c);
+        System.out.println("long   : " + d);
+        System.out.println("float  : " + e);
+        System.out.println("double : " + f);
+    }
+}
