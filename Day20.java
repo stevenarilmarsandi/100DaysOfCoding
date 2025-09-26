a08fbc69327b7b4a8a2b85d6a0a7e12bd784a401
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-26 09:25:17 -0700

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,26 @@
+public class Kelas {
+    public static void main(String[] args) {
+       
+
+            int Bilanganbulat = 969;
+        double Bilangandesimal = 18.6;
+
+        String String1 = String.valueOf(Bilanganbulat);
+        String String2 = String.valueOf(Bilangandesimal);
+
+        System.out.println("Integer ke String: " + String1);
+        System.out.println("Integer ke String: " + String2);
+        
+        String String3 = Bilanganbulat + "";
+        String String4 = Bilangandesimal + "";
+
+        System.out.println("Integer ke String: " + String3);
+        System.out.println("Double ke String: " + String4);
+
+    }
+}
+
+    
+    
+    
+
