73f37e531b1b3b842aab42e7d34231f07048725c
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-26 22:00:32 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,17 @@
+public class mein {
+    public static void main(String[] args) {
+        double a = 20.35;
+        float b = (float) a;
+        long c = (long) b;
+        int d = (int) c;
+        short e = (short) d;
+        byte f = (byte) e;
+
+        System.out.println("double: " + a);
+        System.out.println("float: " + b);
+        System.out.println("long: " + c);
+        System.out.println("int: " + d);
+        System.out.println("short: " + e);
+        System.out.println("byte: " + f);
+    }
+}
