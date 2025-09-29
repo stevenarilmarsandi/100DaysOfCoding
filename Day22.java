91a25fce1a796ea32fefb87fd2516fd66aa0e80f
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-29 06:32:27 -0700

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BilanganBulat {
+ public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       System.out.print("Masukkan angkanya bang : ");
+       double sisi = in.nextDouble();
+       System.out.println("Luas = " + (sisi*sisi));
+       
+       
+        
+    }
+    
+}
