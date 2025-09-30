144e1a880692c4bf8817f85b64c7553c093c792d
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-09-30 07:03:41 -0700

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,14 @@
+public class mueheheehe{
+ public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       System.out.print("Masukkan panjangnya bang : ");
+       double panjang = in.nextDouble();
+       System.out.print("Masukkan lebarnya bang : ");
+       double lebar = in.nextDouble();
+       System.out.println("Luas Panjang = " + (panjang*lebar));
+       
+       
+        
+    }
+    
+}
