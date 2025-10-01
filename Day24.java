a587186e5fc1942e0e1ceb1013e6a1ef51c267c2
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-01 05:32:03 -0700

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BilanganBulat {
+ public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       System.out.print("Masukkan jari jarinya : ");
+       double r = in.nextDouble();
+       System.out.println("Luas Lingkaran = " + (3.14*r*r));
+       
+       
+        
+    }
+    
+}
