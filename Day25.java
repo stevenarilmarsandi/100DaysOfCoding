fe6772c1e6132eecae1ad482a69fa4bf46bbcaf5
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-02 06:26:51 -0700

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,26 @@
+public class JavaApplication25 {
+public static void main(String[] args) {
+       int s = 3;
+       //increment
+       System.out.println("Nilai awal s: " + s);
+
+        System.out.println("s++ = " + s++); 
+        System.out.println("Setelah s++ , s = " + s);
+
+        System.out.println("++s = " + ++s); 
+        System.out.println("Setelah ++s , s = " + s);
+        
+        int w = 4;
+        //decrement
+         System.out.println("Nilai awal w: " + w);
+
+        System.out.println("w-- = " + w--); 
+        System.out.println("Setelah w-- , w = " + w);
+
+        System.out.println("--w = " + --w); 
+        System.out.println("Setelah --w , w = " + w);
+        
+        
+    }
+    
+}
