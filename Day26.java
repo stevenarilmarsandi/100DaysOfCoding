4ba863d55cf799db4e54374b592962293fff18e5
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-03 06:45:41 -0700

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,24 @@
+public class Operatorpenugasan15 {
+    public static void main(String[] args) {
+       
+        int a = 8;
+        int b = 8;
+        int c = 8;
+        int d = 8;
+        int e = 8;
+        
+   
+        
+        System.out.println("Nilai awal a = " + a);
+        a += 4;
+        System.out.println("Setelah Ditambah = "+ a);
+        b -= 4;
+        System.out.println("Setelah Dikurang = "+b);
+        c *= 4;
+        System.out.println("Setelah Dikali = "+c);
+        d /= 4;
+        System.out.println("Setelah Dibagi = "+d);
+                e %= 4;
+        System.out.println("Setelah Dimodulus = "+e);
+    }
+}
