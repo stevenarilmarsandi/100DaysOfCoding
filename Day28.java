d5576433d23310dfb3b1b120186abf123a380966
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-05 06:34:33 -0700

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,16 @@
+public class NewClass1{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+      
+        // Operator ==
+        System.out.println("a == a: " + (a == a)); // true
+        System.out.println("a == b: " + (a == b)); // false
+
+        // Operator !=
+        System.out.println("a != a: " + (a != a)); // false
+        System.out.println("a != b: " + (a != b)); // true
+    }
+}
+
+    
