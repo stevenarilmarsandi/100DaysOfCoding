1a695b323d4a3bcf7eb7c3cce95df56cba7ad983
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-05 17:29:30 -0700

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,14 @@
+public class NewClass1{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+      
+      // Operator < (Lebih Kecil Dari)
+        System.out.println("a < b : " + (a < b));   // true
+        System.out.println("b < a: " + (b < a));   // false
+
+        // Operator > (Lebih Besar Dari)
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("b > a: " + (b > a));   // true
+    }
+}
