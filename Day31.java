a3fdad70d93003cdb7ccbc1cecd2fb627510038c
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-08 06:24:43 -0700

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,14 @@
+public class NewClass1{
+    public static void main(String[] args) {
+        int umur = 18;
+       boolean masihremaja = true;
+      
+      // Operator and &&
+        System.out.println("umur < 20 && masih remaja: " + (umur < 20 && masihremaja));   // true
+   
+
+      
+    }
+}
+
+    
