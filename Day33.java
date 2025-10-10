e79dcfe186b9866c93e759d089a529fb1fcaa9f0
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-10 05:19:09 -0700

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class NewClass1{
+    public static void main(String[] args) {
+     boolean benarsalah = false;
+        System.out.println(! benarsalah);
+        System.out.println( benarsalah);
+    }
+}
+
+    
