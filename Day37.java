0b4837bf959c70729daec5e3f37d3b779c113a34
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-14 04:30:21 -0700

    Day37.java

---
+++ Day37.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pengulangan {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("ANGKA :");
+        int i = in.nextInt();
+            if (i%2 == 0) {
+                System.out.print(i+ " = genap \n");
+            } else if (i%2 == 1){
+                System.out.print(i+" = ganjil \n");
+                
+                
+            
+            }
+        }
