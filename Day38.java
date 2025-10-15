36d53261a3cc291ef544fb589659ded790b8290e
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-15 05:02:15 -0700

    Day38.java

---
+++ Day38.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class King {
+    public static void main(String[] args) {
+        Scanner u = new Scanner(System.in);
+        System.out.print("Masukan angka = ");
+        int a = u.nextInt();
+        if (a > 0){
+            System.out.println(a+ " = positif");
+        }else if (a < 0){
+            
+            System.out.println(a +" = negatif");
+        }else{
+            System.out.println("NOl");
+        } 
+        
+        }
+    }
+    
+
