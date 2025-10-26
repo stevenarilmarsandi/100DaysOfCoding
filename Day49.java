9922a7a3b48ccf0cd9924e4ecb256c31d9ee12bd
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-25 20:10:27 -0700

    Day49.java

--- Day49.java
+++ Day49.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class ConstantaFinal {
+public class Steven
+       {
 public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
     System.out.print("Masukkan nilainya: ");
