797b343d512366082b5df7488a61fa11645e6a36
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-29 09:20:51 -0700

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,14 @@
+package Steven;
+
+
+
+public class Steven{
+   public static void main(String[] args) {
+   for (int angka = 1; angka <= 10; angka++) {
+    if (angka == 5) {
+        break;
+    }
+    System.out.println("angka = " + angka);
+}
+   }
+}
