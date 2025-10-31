4e2690a6225535cef29c9f6ddaa3c077d06b75d2
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-10-31 06:42:25 -0700

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,20 @@
+public class Episod2 {
+   public static void main(String[] args) {
+    for (int i = 0; i <= 14; i+=2) {
+            if (i == 6) {
+                    continue;
+            }else if(i == 4){
+                continue;
+            }else if(i == 10){
+                continue;
+            }else if(i == 12){
+                continue;
+            }else if(i == 14){
+                continue;
+            }
+            System.out.println("Angka : " + i);
+        }
+    }
+    
+}
+    
