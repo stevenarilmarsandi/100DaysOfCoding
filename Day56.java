bd6e649a58d107b9bffeb1e82a7cbe8d616235c2
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-11-02 09:39:12 -0800

    Day56.java

---
+++ Day56.java
@@ -0,0 +1,9 @@
+public class Stvee{
+    public static void main(String[] args) {
+          for (int p = 1; p <= 3; p+=2) { 
+            for (int s = 1; s <= 2; s++) { 
+                System.out.println("angka " + p+ "," + s);
+            }
+        }
+    }
+}
