2859214c568c6fa37b4d0397851198fc6fddfdfc
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-11-02 04:54:28 -0800

    Day55.java

---
+++ Day55.java
@@ -0,0 +1,12 @@
+public class Saya {
+    public static void main(String[] args) {
+         int angka = 55;
+
+        do {
+            System.out.println(angka);
+            angka--;
+        } 
+        while (angka >= 0);
+    }
+}
+   
