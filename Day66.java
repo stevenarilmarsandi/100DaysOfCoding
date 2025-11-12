ce2ce84abb90c0de0f0cd6dea2a8ca397f236afe
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-11-12 04:46:23 -0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class me {
+public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+
+   int n;
+    System.out.print("Masukkan : ");
+    n  = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(".");
+        }
+    }
+}
