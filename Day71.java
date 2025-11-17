18f99c70e44f57a87a2327354781c26c80abb14b
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-11-17 04:38:50 -0800

    Day71.java

---
+++ Day71.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class steven{
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print(" ");
+        int kosong = s.nextInt();
+        
+        for (int i = 1; i < kosong; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(".");
+            }
+            System.out.println("");
+        }
