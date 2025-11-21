203e35e9c806e0eaa6761063e75ac82a40e29b1c
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-11-21 03:52:37 -0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,11 @@
+public class Steven{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String Steven = "Steven Immortal Season ini";
+
+        System.out.println("Panjang string : " + Steven.length());
+        System.out.println("Huruf besar    : " + Steven.toUpperCase());
+        System.out.println("Huruf kecil    : " + Steven.toLowerCase());
+    }
+    
+}
