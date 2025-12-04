1e29971d77ebc9900509d47ce35f5528d3e104d0
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-04 19:43:31 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,11 @@
+public class Steven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    
+       int[] isi = {1,3,8,4,9,4};
+       
+        for (int i : isi) {
+            System.out.println( "isi arraynya: "+ i);
+        }
+    }
+}
