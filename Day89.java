b9c458f6b64de6f5d6f49f44b0a3baf7cd32abce
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-05 06:56:29 -0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,14 @@
+public class Steven{
+    public static void main(String[] args) {
+       Steven ini = new Steven();
+    ini.outprint();
+ ini.Nama("Steven");
+        }
+    public void outprint() {
+    System.out.println("out print");
+}
+    public void Nama(String nama) {
+        System.out.println("Nama gua: " + nama);
+    }
+
+    }
