041db0a57ca0876e1abd0b87d5e9c0e5377f965f
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-06 06:58:38 -0800

    Day90.java

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class Steven{
+public static int hehehe(int a, int b) {
+    return a + b;
+}
+
+public static void main(String[] args) {
+    int hasil = hehehe(9,0);
+    System.out.println( hasil);
+}
+}
