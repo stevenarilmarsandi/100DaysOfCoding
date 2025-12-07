d85cfd389d296aa5c4e9a4e56e14347234d723c2
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-07 23:30:56 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,9 @@
+public class Steven {
+    public static void steven(String nama) {
+        System.out.print(nama);
+    }
+
+    public static void main(String[] args) {
+        steven("Steven");
+    }
+}
