0f8d0a64ca340a765062a9dab39c1331a8157291
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-09 02:42:03 -0800

    Day93.java

---
+++ Day93.java
@@ -0,0 +1,11 @@
+public class Steven{
+
+
+public static void main(String[] args) {
+    double nilai = 7.7;
+
+        System.out.println("Hasil : " + Math.ceil(nilai));
+        System.out.println("Hasil: " + Math.floor(nilai));
+        System.out.println("Hasil: " + Math.round(nilai));
+    }
+}
