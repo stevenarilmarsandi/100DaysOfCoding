6d0f65099b9b94e167e7abee70d4bd994db45a0a
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-11 07:42:32 -0800

    Day95.java

---
+++ Day95.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Steven{
+ public static void info(String ini) {
+        System.out.println( ini);
+      
+        
+    }
+
+   
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        String ini = in.nextLine();
+      info(ini);
+    }
+}
