3f66629277681e2804067b7a669bec2a3d8e0c5b
stevenarilmarsandi <stevenarilmarsandi@gmail.com>
2025-12-14 07:20:54 -0800

    Day98.java

---
+++ Day98.java
@@ -0,0 +1,11 @@
+int n = in.nextInt();
+int a = 1, b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
